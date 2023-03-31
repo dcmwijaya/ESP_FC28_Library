a2aff28cc8e5572744fa9c90d2cffbf7a68d9e9d
devan <mudrawijaya@gmail.com>
2023-03-31 13:36:55 +0700

    version: 1.0.0

--- example/basic_fc28/basic_fc28.ino
+++ example/basic_fc28/basic_fc28.ino
@@ -1,17 +1,18 @@
 // Library: Soil Moisture FC-28
 // By: Devan Cakra M.W
 
-#include <ESP32_FC28.h>
-#define SOIL_MOISTURE_PIN 34
-float kelembapan_tanah;
+#include <ESP32_FC28.h> // Library declaration
+#define SensorFC28_Pin 34 // Sensor pin declaration
+float soil_moisture; // Variable declaration
 
-FC28Sensor mySensor; 
+FC28Sensor mySensor; // Calls the FC28Sensor class declared as the mySensor object.
 
 void setup() {
-  mySensor.initFC28Sensor(115200, SOIL_MOISTURE_PIN);
+  // Initialize this sensor through the initFC28Sensor method. It is accessed from the mySensor object.
+  mySensor.initFC28Sensor(115200, SensorFC28_Pin); // Default baudrate for ESP32:115200 & FC28 Sensor Pin used: 34
 }
 
 void loop() {
-  kelembapan_tanah = mySensor.getSoilMoisture();
-  Serial.println(kelembapan_tanah);
+  soil_moisture = mySensor.getSoilMoisture(); // Calls soil moisture data through the getSoilMoisture method. It is accessed from the mySensor object.
+  Serial.println("Soil Moisture Value: "+String(soil_moisture,2)); // Prints the soil moisture value
 }
