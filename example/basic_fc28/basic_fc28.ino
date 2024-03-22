8901c8b5c6e7b0c03dea58635207162ac4d05bda
Devan C. M. Wijaya, S.Kom <54527592+devancakra@users.noreply.github.com>
2024-03-22 07:49:21 +0700

    Update basic_fc28.ino

--- example/basic_fc28/basic_fc28.ino
+++ example/basic_fc28/basic_fc28.ino
@@ -2,15 +2,16 @@
 // By: Devan Cakra M.W
 
 #include "ESP_FC28.h" // library declaration
-FC28Sensor fc28; // create a new object with the name fc28 to hold the FC28Sensor class
 
-#define SensorFC28_Pin 15 // fc-28 pin declaration
+#define FC28Pin 15 // fc-28 pin declaration
 #define wetSoil 60 // define the minimum value that is considered as 'wet' soil
 #define drySoil 40 // define the maximum value that is considered as 'dry' soil
 
+FC28Sensor fc28(FC28Pin); // create a new object with the name fc28 to hold the FC28Sensor class
+
 void setup() {
   Serial.begin(115200); // default baudrate for the ESP board
-  fc28.begin(SensorFC28_Pin); // initiate fc-28 sensor
+  fc28.begin(); // initiate fc-28 sensor
 }
 
 void loop() {
