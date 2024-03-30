6b03c6b43545b6dace6b7ddbc0a63076ee7ab9af
Devan C. M. Wijaya, S.Kom <54527592+devancakra@users.noreply.github.com>
2024-03-30 13:24:10 +0700

    Update basic_fc28.ino

--- example/basic_fc28/basic_fc28.ino
+++ example/basic_fc28/basic_fc28.ino
@@ -6,6 +6,7 @@
 #define FC28PIN 15 // fc-28 pin declaration
 #define wetSoil 60 // define the minimum value that is considered as 'wet' soil
 #define drySoil 40 // define the maximum value that is considered as 'dry' soil
+#define cbrValue 7 // define the value of calibration
 
 FC28Sensor fc28(FC28PIN); // create a new object with the name fc28 to hold the FC28Sensor class
 
@@ -15,6 +16,7 @@ void setup() {
 }
 
 void loop() {
+  fc28.calibration(cbrValue); // this is used to set the calibration of the FC-28 sensor
   fc28.getSoilMoisture(); // calls soil moisture data through the getSoilMoisture method
   fc28.limit(wetSoil,drySoil); // minimum limit for wet and maximum limit for dry
   fc28.viewData(); // print to serial monitor: soil moisture value & status
