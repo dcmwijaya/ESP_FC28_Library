bca659d84288fcca4d3e672051568c92ecfcbd04
Devan C. M. Wijaya, S.Kom <54527592+devancakra@users.noreply.github.com>
2024-03-31 14:23:12 +0700

    Update ESP_FC28.cpp

--- src/ESP_FC28.cpp
+++ src/ESP_FC28.cpp
@@ -19,15 +19,21 @@ float FC28Sensor::calibration(float cbr) {
 }
 
 
+void FC28Sensor::threshold() {
+  if (percentage < 0) { percentage = 0; }     // lower limit
+  if (percentage > 100) { percentage = 100; } // upper limit
+}
+
+
 float FC28Sensor::getSoilMoisture() {
   ADCvalue = analogRead(_pin);
 
-  if(ADCvalue == NULL){
+  if (ADCvalue == NULL) {
     Serial.println("Failed to read the value from the FC-28 sensor !!"); 
     percentage = 0;
     return percentage;
   }
-  else{
+  else {
     #if defined(ESP8266)
       percentage = ((100 - ((ADCvalue/1023) * 100))-adjust); // 10 bit => ADC resolution: 1023
       threshold();
@@ -43,12 +49,6 @@ float FC28Sensor::getSoilMoisture() {
 }
 
 
-void FC28Sensor::threshold() {
-  if(percentage < 0){ percentage = 0; }     // lower limit
-  if(percentage > 100){ percentage = 100; } // upper limit
-}
-
-
 void FC28Sensor::limit(uint8_t min_wet, uint8_t max_dry) {
   wetSoil = min_wet;
   drySoil = max_dry;
@@ -56,21 +56,23 @@ void FC28Sensor::limit(uint8_t min_wet, uint8_t max_dry) {
 
 
 void FC28Sensor::vState() {
-  Serial.println("\nValue: "+String(percentage,2)+"%"); // print to serial monitor: soil moisture value
-  if(percentage >= wetSoil) { // if the sensor value indicates a wet condition then :
-    Serial.println("Status: wet"); // print to serial monitor: wet soil conditions
-  }
-  else if(percentage > drySoil && percentage < wetSoil) { // if the sensor value indicates a moist condition then :
-    Serial.println("Status: moist"); // print to serial monitor: moist soil conditions
-  }
-  else{ // if the sensor value is not in wet and moist conditions then :
-    Serial.println("Status: dry"); // print to serial monitor: dry soil conditions
+  Serial.println("\nValue: "+String(percentage,2)+"%");
+  if (wetSoil != NULL && drySoil != NULL) {
+    if (percentage >= wetSoil) {
+      Serial.println("Status: wet");
+    }
+    else if (percentage > drySoil && percentage < wetSoil) {
+      Serial.println("Status: moist");
+    }
+    else {
+      Serial.println("Status: dry");
+    }
   }
 }
 
 
 void FC28Sensor::viewData() {
-  if(ADCvalue != NULL){
+  if (ADCvalue != NULL) {
     #if defined(ESP8266) 
       vState();
     #elif defined(ESP32)
